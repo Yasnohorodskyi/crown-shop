7c8a041095dc92aa144151506d0e703f98266cc8
Andrii Yasnohorodskyi <yasnogorodskiy@gmail.com>
2023-04-06 15:25:23 +0100

    add two types of auth in google

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,42 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
+import {
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    const getResult = async () => {
+      try {
+        const response = await getRedirectResult(auth);
+        if(response.user) {
+          const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    getResult();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
-  <div>
-    <h1>Sign In Page</h1>
-    <button onClick={logGoogleUser}>Sign In With Google Popup</button>    
-  </div>
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign In With Google Redirect
+      </button>
+    </div>
   );
 };
 export default SignIn;
